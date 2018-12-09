afb6b88db85096f466b28bf289d3454919994097
twelvefish <amingo08131@gmail.com>
2018-12-09 21:46:34 +0800

    fix welcomePic

--- services/lineServices.ts
+++ services/lineServices.ts
@@ -18,12 +18,13 @@ export const welcomeMessage = (event: MemberJoinEvent) => {
                     `\t\t\t\t記事本看下規則，記得按下符號表示看過\uDBC0\uDC09，有任何事情請召喚群管，歡迎一起歡樂開黑玩遊戲～\n\n` +
                     `\t\t\t\t記得動動小手把群組提醒關掉🙈🙈🙈，這裡有點熱鬧\uDBC0\uDC35`
             }
-            const welcomePic: ImageMessage = {
-                type: "image",
-                originalContentUrl: member.pictureUrl,
-                previewImageUrl: member.pictureUrl
-            }
-            pushMessages(groupId, [welcome, welcomePic])
+            // const welcomePic: ImageMessage = {
+            //     type: "image",
+            //     originalContentUrl: member.pictureUrl,
+            //     previewImageUrl: member.pictureUrl
+            // }
+            // pushMessages(groupId, [welcome, welcomePic])
+            pushMessages(groupId, [welcome])
         }).catch(err => console.log(err))
     }
 }
