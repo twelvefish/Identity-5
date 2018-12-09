490ca3b341a7d617849a9e6bec3e4b0eb88ce29c
twelvefish <amingo08131@gmail.com>
2018-12-09 21:38:32 +0800

    heroku config

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Identity-5
 第五人格 Line群組機器人
+git pull
+git add .
+git commit -a -m ""
+git push heroku master
+heroku ps:scale web=1
+heroku logs --tail
--- src/webhook.ts
+++ src/webhook.ts
@@ -9,7 +9,7 @@ var router = express.Router()
 
 router.use(function (req, res, next) {
     console.log("輸出記錄訊息至終端機", req.method, req.url);
-    next();
+    next()
 })
 
 router.post('/webhook', Line.middleware(LineConfig), (req, res) => {
@@ -26,11 +26,14 @@ router.post('/webhook', Line.middleware(LineConfig), (req, res) => {
     })
     Promise
         .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result))
+        .then((result) => {
+            res.json(result)
+            res.status(200).end()
+        })
         .catch((err) => {
-            console.error("err", err);
-            res.status(500).end();
-        });
+            console.error("err", err)
+            res.status(500).end()
+        })
 })
 
 
