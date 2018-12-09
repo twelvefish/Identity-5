93d1f18ef86a676cd8c5f0f55c9732ac8a14c45b
twelvefish <amingo08131@gmail.com>
2018-12-09 20:17:37 +0800

    gitignore && npm run start

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ const webhook = require('./webhook')
 app.use('/', webhook)
 
 app.listen(3000, () => {
-    console.log('Express server listening on port 3000 test')
+    console.log('Express server listening on port 3000')
 })
